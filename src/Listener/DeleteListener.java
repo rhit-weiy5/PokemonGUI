5c42d35a064724333ac84e93a7439c526fe20df3
rhit-weiy5 <weiy5@rose-hulman.edu>
2022-01-27 18:23:40 -0500

    Commit

--- src/Listener/DeleteListener.java
+++ src/Listener/DeleteListener.java
@@ -4,10 +4,13 @@ import Database.DatabaseConnection;
 
 import javax.swing.*;
 import javax.xml.crypto.Data;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Types;
 
 public class DeleteListener implements ActionListener {
 
@@ -21,14 +24,29 @@ public class DeleteListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        PreparedStatement stmt = null;
+        delete();
+    }
+
+    public boolean delete(){
+        CallableStatement stmt = null;
         try {
-            stmt = db.getConnection().prepareStatement("EXEC delete_pokemon @PID = ?");
-            int x = Integer.parseInt(pidTextField.getText());
+            stmt = db.getConnection().prepareCall("{call delete_pokemon (@PID = ?)}");
+            int x = 0;
+
+            try {
+                x = Integer.parseInt(pidTextField.getText());
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null,"PID should be an int");
+                //e.printStackTrace();
+                return false;
+            }
             stmt.setInt(1, x);
             stmt.execute();
+            return true;
+
         } catch (SQLException ex) {
-            ex.printStackTrace();
+            JOptionPane.showMessageDialog(null, "PID not exists");
+            return false;
         }
     }
 }
