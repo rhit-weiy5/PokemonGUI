e8a1b3210a52527043544938f92df0b2e7b0383f
rhit-weiy5 <weiy5@rose-hulman.edu>
2022-01-27 15:55:44 -0500

    Commit

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,16 +1,56 @@
 import java.sql.Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    //DO NOT EDIT THIS STRING, YOU WILL RECEIVE NO CREDIT FOR THIS TASK IF THIS STRING IS EDITED
     private final String SampleURL = "jdbc:sqlserver://${dbServer};databaseName=${dbName};user=${user};password={${pass}}";
+
     private Connection connection = null;
 
     private String databaseName;
     private String serverName;
 
     public DatabaseConnection(String serverName, String databaseName) {
+        //DO NOT CHANGE THIS METHOD
         this.serverName = serverName;
         this.databaseName = databaseName;
     }
 
+    public boolean connect(String user, String pass) {
+        //DONE: Task 1
+        //BUILD YOUR CONNECTION STRING HERE USING THE SAMPLE URL ABOVE
+        String fullUrl = SampleURL
+                .replace("${dbServer}", this.serverName)
+                .replace("${dbName}", this.databaseName)
+                .replace("${user}", user)
+                .replace("${pass}", pass);
+        System.out.println("Connecting");
+        try {
+            connection = DriverManager.getConnection(fullUrl);
+            System.out.println("Connection Successful");
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+    public Connection getConnection() {
+        return this.connection;
+    }
+
+    public void closeConnection() {
+        //DONE: Task 1
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,14 @@
 public class main {
+
+    private static String serverName = "titan.csse.rose-hulman.edu";
+    private static String databaseName = "PokémonDB";
+    private static String username = "";
+    private static String password = "";
+
     public static void main(String[] args){
         Frame frame = new Frame();
+        DatabaseConnection db = new DatabaseConnection(serverName, databaseName);
+        db.connect(username, password);
     }
 
 }
