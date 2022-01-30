e5a2612b503599838fe34c9766ced9db6236c4c5
rhit-weiy5 <weiy5@rose-hulman.edu>
2022-01-29 20:10:44 -0500

    Commit

--- src/Panel/PokemonPanel.java
+++ src/Panel/PokemonPanel.java
@@ -37,13 +37,13 @@ public class PokemonPanel extends JPanel {
     	Statement stmt;
 		try {		
 			stmt = this.db.getConnection().createStatement();
-			ResultSet countrs = stmt.executeQuery("SELECT PID From Pokemon");
+			ResultSet countrs = stmt.executeQuery("SELECT PID From Pokemon1");
 			int count=0;
 			while(countrs.next()) {
 				count++;
 			}
 			
-			ResultSet rs = stmt.executeQuery("SELECT PID, Pokemon.Name as Pname, Pokemon.Gender as Pgender, Level, Friendship, Species.SpecieName as SName, Ability.Name as AName, Trainer.Name as TName From Pokemon JOIN Trainer on Trainer.ID = Pokemon.TrainerID JOIN Ability on Pokemon.AbilityID = Ability.ID Join Species on Pokemon.SpecieID = Species.ID");
+			ResultSet rs = stmt.executeQuery("SELECT PID, Pokemon1.Name as Pname, Pokemon1.Gender as Pgender, Level, Friendship, Pokedex.Name as SName, Ability1.Name as AName, Trainer.Name as TName From Pokemon1 JOIN Trainer on Trainer.ID = Pokemon1.TrainerID JOIN Ability1 on Pokemon1.AbilityID = Ability1.ID Join Pokedex on Pokemon1.SpeciesID = Pokedex.ID");
 			String[][] rec = new String[count][8];
 			String[] header = { "PID", "Name", "Gender", "Level", "Friendship", "SpecieName", "AbilityName", "TrainerName"};
 			int index =0;
@@ -118,9 +118,9 @@ public class PokemonPanel extends JPanel {
     	this.specieComboBox.addItem("None");
 		try {
 			Statement stmt = this.db.getConnection().createStatement();
-			ResultSet rs = stmt.executeQuery(" SELECT DISTINCT SpecieName From Pokemon JOIN Species on Species.ID = Pokemon.SpecieID");
+			ResultSet rs = stmt.executeQuery(" SELECT DISTINCT Pokemon1.Name From Pokemon1 JOIN Pokedex on Pokedex.ID = Pokemon1.SpeciesID");
 			while(rs.next()) {
-				this.specieComboBox.addItem(rs.getString("SpecieName"));
+				this.specieComboBox.addItem(rs.getString("Name"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -130,7 +130,7 @@ public class PokemonPanel extends JPanel {
     	this.trainerTextField.addItem("None");
 		try {
 			Statement stmt = this.db.getConnection().createStatement();
-			ResultSet rs = stmt.executeQuery(" SELECT DISTINCT Trainer.Name as trainername From Pokemon JOIN Trainer on Trainer.ID = Pokemon.TrainerID");
+			ResultSet rs = stmt.executeQuery(" SELECT DISTINCT Trainer.Name as trainername From Pokemon1 JOIN Trainer on Trainer.ID = Pokemon1.TrainerID");
 			while(rs.next()) {
 				this.trainerTextField.addItem(rs.getString("trainername"));
 
